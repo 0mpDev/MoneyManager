ea07daa3489b5bfc71e51821fd71e0da93a186f4
0mpDev <patniom3@gmail.com>
2025-09-24 18:33:10 +0530

    Commit frontend updates

--- frontend/moneymanager/src/context/AppContext.jsx
+++ frontend/moneymanager/src/context/AppContext.jsx
@@ -1,19 +1,24 @@
-import { createContext, useState } from "react";
-
+import {createContext, useState} from "react";
 
 export const AppContext = createContext();
 
-export const AppContextProvider = ({ children }) => {
+export const AppContextProvider = ({children}) => {
+
     const [user, setUser] = useState(null);
+
+    const clearUser = () => {
+        setUser(null);
+    }
 
     const contextValue = {
         user,
-        setUser
-    };
+        setUser,
+        clearUser
+    }
 
     return (
         <AppContext.Provider value={contextValue}>
             {children}
         </AppContext.Provider>
-    );
-};
+    )
+}
--- frontend/moneymanager/src/util/uploadProfileImage.js
+++ frontend/moneymanager/src/util/uploadProfileImage.js
@@ -1,11 +1,11 @@
-import { API_ENDPOINTS } from "../util/apiEndpoints";
+import {API_ENDPOINTS} from "./apiEndpoints.js";
 
-const CLOUDINARY_CLOUD_PRESET = "moneymanager";
+const CLOUDINARY_UPLOAD_PRESET = "moneymanager";
 
-const uploadProfileImage = async (file) => {
+const uploadProfileImage = async (image) => {
     const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", CLOUDINARY_CLOUD_PRESET);
+    formData.append("file", image);
+    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
 
     try {
         const response = await fetch(API_ENDPOINTS.UPLOAD_IMAGE, {
@@ -13,21 +13,18 @@ const uploadProfileImage = async (file) => {
             body: formData
         });
 
-        // read response as text first
-        const text = await response.text();
-        const data = text ? JSON.parse(text) : {};
-
         if (!response.ok) {
             const errorData = await response.json();
-            throw new Error(`Failed to upload image: ${data?.error?.message || response.statusText}`);
+            throw new Error(`Cloudinary upload failed: ${errorData.error.message || response.statusText}`);
         }
 
-        console.log("Image uploaded successfully:", data);
+        const data = await response.json();
+        console.log('Image uploaded successfully.', data);
         return data.secure_url;
     } catch (error) {
-        console.error("Error uploading image:", error);
+        console.error("Error uploading the image", error);
         throw error;
     }
-};
+}
 
 export default uploadProfileImage;
